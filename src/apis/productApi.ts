cbe6ce767bea6025e6f35fb51e0583b1a4a16654
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-03 21:19:13 +0700

    setup: Api Layer

---
+++ src/apis/productApi.ts
@@ -0,0 +1,10 @@
+import axiosClient from './axiosClient';
+
+const productApi = {
+  getAll() {
+    const url = 'products';
+    return axiosClient.get(url);
+  },
+};
+
+export default productApi;
--- src/features/product/components/ProductList.tsx
+++ src/features/product/components/ProductList.tsx
@@ -1,6 +1,17 @@
+import productApi from '@/apis/productApi';
 import { Button } from '@mui/material';
+import { useEffect } from 'react';
 
 function ProductList() {
+  const fetchProducts = async () => {
+    const res = await productApi.getAll();
+    console.log('check res', res);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Button variant='contained' color='success'>
