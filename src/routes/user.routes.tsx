b539dd8a6ca7fdee1ce7b28238d5630701bcc4f9
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-04 23:08:18 +0700

    feat: fetch user when reload

--- src/routes/user.routes.tsx
+++ src/routes/user.routes.tsx
@@ -1,8 +1,37 @@
 import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import authApi from '@/apis/authApi';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@/redux/user/user.slice';
 
 function UserRoutes() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['me'],
+    queryFn: authApi.getMe,
+  });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      setUser({
+        firstName: data?.firstName,
+        lastName: data?.lastName,
+        avatar: data?.avatar,
+        email: data?.email,
+        role: data?.role,
+      })
+    );
+  }, [dispatch, data]);
+
+  if (isLoading) return <>Loading...</>;
+  if (error) return <>Error...</>;
+
+  console.log(data);
+
   return (
     <div>
       <Header />
