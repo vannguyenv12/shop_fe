4aada784cc7f60f45e5b0e48b09d1bf0a210a4a2
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-05 21:11:32 +0700

    feat: useAuthenticate hook

---
+++ src/hooks/useAuthenticate.tsx
@@ -0,0 +1,44 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { clearUser, setUser } from '@/redux/user/user.slice';
+import useGetMeQuery from '@/hooks/useGetMeQuery';
+import { useAppSelector } from '@/redux/hook';
+import { useNavigate } from 'react-router-dom';
+
+function useAuthenticate() {
+  const { data, isLoading, error } = useGetMeQuery();
+  const user = useAppSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data && !user.isAuthenticated) {
+      dispatch(
+        setUser({
+          firstName: data.firstName,
+          lastName: data.lastName,
+          avatar: data.avatar,
+          email: data.email,
+          role: data.role,
+        })
+      );
+    }
+  }, [dispatch, data, user.isAuthenticated]);
+
+  useEffect(() => {
+    if (!isLoading && !user.isAuthenticated && !data) {
+      navigate('/sign-in');
+    }
+  }, [user, navigate, isLoading, data]);
+
+  useEffect(() => {
+    if (error) {
+      dispatch(clearUser());
+    }
+  }, [error, dispatch]);
+
+  return { isLoading, error };
+}
+
+export default useAuthenticate;
--- src/routes/user.routes.tsx
+++ src/routes/user.routes.tsx
@@ -1,44 +1,10 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { clearUser, setUser } from '@/redux/user/user.slice';
-import useGetMeQuery from '@/hooks/useGetMeQuery';
-import { useAppSelector } from '@/redux/hook';
+import useAuthenticate from '@/hooks/useAuthenticate';
 
 function UserRoutes() {
-  const { data, isLoading, error } = useGetMeQuery();
-  const user = useAppSelector((state) => state.user);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (data && !user.isAuthenticated) {
-      dispatch(
-        setUser({
-          firstName: data.firstName,
-          lastName: data.lastName,
-          avatar: data.avatar,
-          email: data.email,
-          role: data.role,
-        })
-      );
-    }
-  }, [dispatch, data, user.isAuthenticated]);
-
-  useEffect(() => {
-    if (!isLoading && !user.isAuthenticated && !data) {
-      navigate('/sign-in');
-    }
-  }, [user, navigate, isLoading, data]);
-
-  useEffect(() => {
-    if (error) {
-      dispatch(clearUser());
-    }
-  }, [error, dispatch]);
+  const { isLoading, error } = useAuthenticate();
 
   if (isLoading) return <>Loading...</>;
   if (error) {
