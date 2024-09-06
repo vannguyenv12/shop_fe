97be0b4b97fbd3bbc3ffb29681564e7640294253
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-06 20:42:56 +0700

    ui: list sidebar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,5 @@
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return <div style={{ marginLeft: '300px' }}>Dashboard</div>;
 }
 
 export default Dashboard;
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,37 +6,44 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import CategoryIcon from '@mui/icons-material/Category';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+
+const sidebarList = [
+  {
+    name: 'Category',
+    icon: <CategoryIcon />,
+  },
+  {
+    name: 'Product',
+    icon: <InventoryIcon />,
+  },
+  {
+    name: 'Order',
+    icon: <ShoppingCartIcon />,
+  },
+  {
+    name: 'User',
+    icon: <PersonIcon />,
+  },
+];
 
 export default function Sidebar() {
   const DrawerList = (
     <Box sx={{ width: 250 }} role='presentation'>
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {sidebarList.map((item) => (
+          <ListItem key={item.name} disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
     </Box>
   );
 
