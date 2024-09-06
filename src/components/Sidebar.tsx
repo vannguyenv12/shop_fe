30616219d25d903f2d492be590d562d8740efd74
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-06 21:09:38 +0700

    ui: category component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,33 +10,47 @@ import InventoryIcon from '@mui/icons-material/Inventory';
 import CategoryIcon from '@mui/icons-material/Category';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PersonIcon from '@mui/icons-material/Person';
+import { useNavigate } from 'react-router-dom';
 
 const sidebarList = [
   {
     name: 'Category',
     icon: <CategoryIcon />,
+    link: '/admin/category',
   },
   {
     name: 'Product',
     icon: <InventoryIcon />,
+    link: '/admin/product',
   },
   {
     name: 'Order',
     icon: <ShoppingCartIcon />,
+    link: '/admin/order',
   },
   {
     name: 'User',
     icon: <PersonIcon />,
+    link: '/admin/user',
   },
 ];
 
 export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleClick = (
+    _event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    url: string
+  ) => {
+    navigate(url);
+  };
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role='presentation'>
       <List>
         {sidebarList.map((item) => (
           <ListItem key={item.name} disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={(e) => handleClick(e, item.link)}>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.name} />
             </ListItemButton>
--- src/features/category/pages/CategoryAdminPage.tsx
+++ src/features/category/pages/CategoryAdminPage.tsx
@@ -1,8 +1,10 @@
+import AddCategoryModal from '../components/AddCategoryModal';
 import CategoryListAdmin from '../components/CategoryListAdmin';
 
 function CategoryAdminPage() {
   return (
     <div style={{ marginLeft: '250px' }}>
+      <AddCategoryModal />
       <CategoryListAdmin />
     </div>
   );
