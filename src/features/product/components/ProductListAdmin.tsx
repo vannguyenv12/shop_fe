c2fa4f77ef6bb55e84db5a44f86ea778ffa7f5b9
vannguyenv12 <npnv.vn1@gmail.com>
2024-09-08 00:43:30 +0700

    feat: category name

--- src/features/product/components/ProductListAdmin.tsx
+++ src/features/product/components/ProductListAdmin.tsx
@@ -1,68 +1,82 @@
 import Box from '@mui/material/Box';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import useProductsQuery from '../hooks/useProductsQuery';
-
-const columns: GridColDef<IProduct>[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'name',
-    headerName: 'Name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'shortDescription',
-    headerName: 'Short Description',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'quantity',
-    headerName: 'Quantity',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'price',
-    headerName: 'Price',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'main_image',
-    headerName: 'Image',
-    sortable: false,
-    width: 160,
-    renderCell(params) {
-      return (
-        <img
-          src={`${import.meta.env.VITE_BACKEND_URL}/images/products/${
-            params.value
-          }`}
-          width={50}
-          height={50}
-        />
-      );
-    },
-  },
-  {
-    field: 'categoryId',
-    headerName: 'Category ID',
-    sortable: false,
-    width: 160,
-  },
-  {
-    field: 'shopId',
-    headerName: 'Shop ID',
-    sortable: false,
-    width: 160,
-  },
-];
+import useCategoriesQuery from '@/features/category/hooks/useCategoriesQuery';
 
 export default function ProductListAdmin() {
+  const columns: GridColDef<IProduct>[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'name',
+      headerName: 'Name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'shortDescription',
+      headerName: 'Short Description',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      type: 'number',
+      width: 110,
+      editable: true,
+    },
+    {
+      field: 'main_image',
+      headerName: 'Image',
+      sortable: false,
+      width: 160,
+      renderCell(params) {
+        return (
+          <img
+            src={`${import.meta.env.VITE_BACKEND_URL}/images/products/${
+              params.value
+            }`}
+            width={50}
+            height={50}
+          />
+        );
+      },
+    },
+    {
+      field: 'categoryId',
+      headerName: 'Category Name',
+      sortable: false,
+      width: 160,
+      renderCell(params) {
+        const categoryId = params.value;
+        const category = getCategoryName(categoryId);
+
+        return category ? category.name : '';
+      },
+    },
+    {
+      field: 'shopId',
+      headerName: 'Shop ID',
+      sortable: false,
+      width: 160,
+    },
+  ];
+
   const { data, isLoading, error } = useProductsQuery();
+  const { data: categoriesData } = useCategoriesQuery();
+
+  const getCategoryName = (categoryId: number) => {
+    const categories = categoriesData.data;
+
+    return categories.find((cat) => cat.id === categoryId);
+  };
 
   const products = data.data;
 
